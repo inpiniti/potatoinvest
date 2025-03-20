2e7bf588d8bd49fee878fd91fd61e91e7f35e416
ykjung <ykjung@scgs.co.kr>
2025-03-20 12:58:32 +0900

    Fix typo in Password component label and description; standardize import quotes

--- app/page/setting/Password.tsx
+++ app/page/setting/Password.tsx
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
-import { Input } from "@/components/ui/input";
-import { tempKeyStore } from "@/store/tempKeyStore";
+import { Input } from '@/components/ui/input';
+import { tempKeyStore } from '@/store/tempKeyStore';
 
 const Password = () => {
   const { key, setKey } = tempKeyStore();
@@ -9,9 +9,9 @@ const Password = () => {
   return (
     <section className="bg-white border rounded-sm overflow-hidden p-4 flex justify-between items-center gap-4">
       <div>
-        <label className="font-bold">암호화용 sort</label>
+        <label className="font-bold">암호화용 salt</label>
         <div className="text-xs text-neutral-500">
-          데이터 암, 복호화를 위한 sort입니다.
+          데이터 암, 복호화를 위한 salt입니다.
         </div>
       </div>
       <Input
