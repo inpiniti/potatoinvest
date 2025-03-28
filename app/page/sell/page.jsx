589f2dd4b5bc7bfe7abe2425322808d083d07ef7
ykjung <ykjung@scgs.co.kr>
2025-03-28 12:31:16 +0900

    refactor: add error handling to getList function in Sell component

--- app/page/sell/page.jsx
+++ app/page/sell/page.jsx
@@ -25,44 +25,48 @@ const Sell = () => {
   const [expandedRows, setExpandedRows] = useState({}); // 추가된 상태
 
   const getList = useCallback(async () => {
-    alert('cano : ' + String(cano));
-    alert('acntPrdtCd : ' + String(acntPrdtCd));
-    if (cano && acntPrdtCd) {
-      const payload = {
-        CANO: cano,
-        ACNT_PRDT_CD: acntPrdtCd,
-        OVRS_EXCG_CD: 'NASD',
-        NATN_CD: '', // 국가코드 : 공란
-        CRCY_CD: '', // 통화코드 : 공란
-        PDNO: '', // 상품번호 : 공란
-        INQR_STRT_DT: dayjs('2025.03.25').format('YYYYMMDD'), // 조회시작일자
-        INQR_END_DT: dayjs().format('YYYYMMDD'), // 조회종료일자
-        WCRC_FRCR_DVSN_CD, // 원화외화구분코드 : 01: 외화, 02: 원화
-        CTX_AREA_FK200: '', // 연속조회검색조건200 : 공란
-        CTX_AREA_NK200: '', // 연속조회키200 : 공란
-      };
+    try {
+      alert('cano : ' + String(cano));
+      alert('acntPrdtCd : ' + String(acntPrdtCd));
+      if (cano && acntPrdtCd) {
+        const payload = {
+          CANO: cano,
+          ACNT_PRDT_CD: acntPrdtCd,
+          OVRS_EXCG_CD: 'NASD',
+          NATN_CD: '', // 국가코드 : 공란
+          CRCY_CD: '', // 통화코드 : 공란
+          PDNO: '', // 상품번호 : 공란
+          INQR_STRT_DT: dayjs('2025.03.25').format('YYYYMMDD'), // 조회시작일자
+          INQR_END_DT: dayjs().format('YYYYMMDD'), // 조회종료일자
+          WCRC_FRCR_DVSN_CD, // 원화외화구분코드 : 01: 외화, 02: 원화
+          CTX_AREA_FK200: '', // 연속조회검색조건200 : 공란
+          CTX_AREA_NK200: '', // 연속조회키200 : 공란
+        };
 
-      alert('payload : ' + JSON.stringify(payload));
+        alert('payload : ' + JSON.stringify(payload));
 
-      const response = await api.trading.inquirePeriodProfit(payload);
+        const response = await api.trading.inquirePeriodProfit(payload);
 
-      alert('response : ' + JSON.stringify(response));
+        alert('response : ' + JSON.stringify(response));
 
-      const data = await response.json();
+        const data = await response.json();
 
-      alert('data : ' + JSON.stringify(data));
+        alert('data : ' + JSON.stringify(data));
 
-      setList(
-        data?.output1?.map((item) => {
-          const logo = logos.find(
-            (logo) => logo.name === item.ovrs_pdno
-          ).logoid;
-          return {
-            ...item,
-            logo,
-          };
-        })
-      );
+        setList(
+          data?.output1?.map((item) => {
+            const logo = logos.find(
+              (logo) => logo.name === item.ovrs_pdno
+            ).logoid;
+            return {
+              ...item,
+              logo,
+            };
+          })
+        );
+      }
+    } catch (error) {
+      alert(error);
     }
   }, []);
 
