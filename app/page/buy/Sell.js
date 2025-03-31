db761c29c7fc03730512e40a5a0cf27df69985e9
ykjung <ykjung@scgs.co.kr>
2025-03-31 13:02:36 +0900

    개별 매도기능

---
+++ app/page/buy/Sell.js
@@ -0,0 +1,124 @@
+'use client';
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+
+const Sell = ({
+  ovrs_pdno,
+  ovrs_item_name,
+  pchs_avg_pric,
+  ovrs_cblc_qty,
+  evlu_pfls_rt,
+}) => {
+  const { 매도 } = useTrading();
+
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Drawer open={isOpen} onOpenChange={setIsOpen}>
+      <DrawerTrigger asChild>
+        <Button>매도</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>{ovrs_pdno}</DrawerTitle>
+          <DrawerDescription>{ovrs_item_name}</DrawerDescription>
+        </DrawerHeader>
+        <div className="px-4 flex gap-2">
+          <div>매입가 : {pchs_avg_pric}, </div>
+          <div>수량 : {ovrs_cblc_qty}, </div>
+          <div>수익률 : {evlu_pfls_rt}</div>
+        </div>
+        <div className="px-4 flex flex-col gap-2 py-4">
+          <div className="flex gap-2">
+            <Button
+              className="bg-blue-400"
+              onClick={() =>
+                매도({
+                  ovrs_pdno,
+                  ovrs_cblc_qty,
+                  now_pric2: (pchs_avg_pric * 1.02).toFixed(2),
+                })
+              }
+            >
+              2%({(pchs_avg_pric * 1.02).toFixed(2)})에 매도
+            </Button>
+            <Button className="bg-red-400">
+              -2%({(pchs_avg_pric * 0.98).toFixed(2)})에 매수
+            </Button>
+          </div>
+          <div className="flex gap-2">
+            <Button
+              className="bg-blue-400"
+              onClick={() =>
+                매도({
+                  ovrs_pdno,
+                  ovrs_cblc_qty,
+                  now_pric2: (pchs_avg_pric * 1.05).toFixed(2),
+                })
+              }
+            >
+              5%({(pchs_avg_pric * 1.05).toFixed(2)})에 매도
+            </Button>
+            <Button className="bg-red-400">
+              -5%({(pchs_avg_pric * 0.95).toFixed(2)})에 매수
+            </Button>
+          </div>
+          <div className="flex gap-2">
+            <Button
+              className="bg-blue-400"
+              onClick={() =>
+                매도({
+                  ovrs_pdno,
+                  ovrs_cblc_qty,
+                  now_pric2: (pchs_avg_pric * 1.1).toFixed(2),
+                })
+              }
+            >
+              10%({(pchs_avg_pric * 1.1).toFixed(2)})에 매도
+            </Button>
+            <Button className="bg-red-400">
+              -10%({(pchs_avg_pric * 0.9).toFixed(2)})에 매수
+            </Button>
+          </div>
+          <div className="flex gap-2">
+            <Button
+              className="bg-blue-400"
+              onClick={() =>
+                매도({
+                  ovrs_pdno,
+                  ovrs_cblc_qty,
+                  now_pric2: (pchs_avg_pric * 1.3).toFixed(2),
+                })
+              }
+            >
+              30%({(pchs_avg_pric * 1.3).toFixed(2)})에 매도
+            </Button>
+            <Button className="bg-red-400">
+              -30%({(pchs_avg_pric * 0.7).toFixed(2)})에 매수
+            </Button>
+          </div>
+        </div>
+        <DrawerFooter>
+          <div className="flex items-center justify-end w-full gap-2">
+            <DrawerClose>
+              <Button variant="outline">취소</Button>
+            </DrawerClose>
+          </div>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default Sell;
--- app/page/buy/page.js
+++ app/page/buy/page.js
@@ -1,14 +1,15 @@
-"use client";
+'use client';
 
-import { Card } from "@/components/ui/card";
+import { Card } from '@/components/ui/card';
 
-import useApi from "@/hooks/useApi";
-import useAccount from "@/hooks/useAccount";
+import useApi from '@/hooks/useApi';
+import useAccount from '@/hooks/useAccount';
 
-import { logos } from "@/json/logoData";
+import { logos } from '@/json/logoData';
 
-import { useCallback, useEffect, useState } from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useCallback, useEffect, useState } from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import Sell from './Sell';
 
 const Buy = () => {
   const api = useApi();
@@ -21,10 +22,10 @@ const Buy = () => {
       const payload = {
         CANO: cano,
         ACNT_PRDT_CD: acntPrdtCd,
-        OVRS_EXCG_CD: "NASD",
-        TR_CRCY_CD: "USD",
-        CTX_AREA_FK200: "",
-        CTX_AREA_NK200: "",
+        OVRS_EXCG_CD: 'NASD',
+        TR_CRCY_CD: 'USD',
+        CTX_AREA_FK200: '',
+        CTX_AREA_NK200: '',
       };
 
       const response = await api.trading.inquireBalance(payload);
@@ -39,8 +40,10 @@ const Buy = () => {
   }, []);
 
   const color = useCallback((item) =>
-    item.evlu_pfls_rt > 0 ? "text-red-400" : "text-blue-400"
+    item.evlu_pfls_rt > 0 ? 'text-red-400' : 'text-blue-400'
   );
+
+  //const { 매도 } = useTrading();
 
   return (
     <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 2xl:grid-cols-3">
@@ -68,15 +71,22 @@ const Buy = () => {
             <div className="flex gap-4">
               <div className="flex flex-col items-end">
                 <div className="text-xs text-neutral-400">
-                  매입평균가격 {Number(item.pchs_avg_pric).toFixed(2)} x{" "}
+                  매입평균가격 {Number(item.pchs_avg_pric).toFixed(2)} x{' '}
                   {item.ovrs_cblc_qty}
                 </div>
                 <div className={`text-2xl font-bold ${color(item)}`}>
-                  {Number(item.now_pric2).toFixed(2)}{" "}
+                  {Number(item.now_pric2).toFixed(2)}{' '}
                   <span className="text-sm">({item.evlu_pfls_rt}%)</span>
                 </div>
               </div>
             </div>
+            <Sell
+              ovrs_pdno={item.ovrs_pdno}
+              ovrs_item_name={item.ovrs_item_name}
+              pchs_avg_pric={Number(item.pchs_avg_pric).toFixed(2)}
+              ovrs_cblc_qty={item.ovrs_cblc_qty}
+              evlu_pfls_rt={item.evlu_pfls_rt}
+            />
           </Card>
         );
       })}
