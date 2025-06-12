db469509162e497aa3eb002dd6763da539d1b0f8
inpiniti <younginpiniti@gmail.com>
2025-06-12 23:15:12 +0900

    fix: update AsideItem styles for improved hover effects and active state

--- app/dashboard/components/AsideItem.jsx
+++ app/dashboard/components/AsideItem.jsx
@@ -10,8 +10,8 @@ const AsideItem = ({ title, date, info, description, onClick, active }) => {
     <a
       href="#"
       className={`box-border ${
-        active && "bg-primary-foreground border border-primary"
-      } flex flex-col items-start gap-2 whitespace-nowrap border-b p-4 text-sm leading-tight hover:bg-primary-foreground hover:text-sidebar-accent-foreground`}
+        active && "bg-white border border-primary"
+      } flex flex-col items-start gap-2 whitespace-nowrap border-b p-4 text-sm leading-tight hover:bg-white hover:border-red-400 hover:border hover:text-sidebar-accent-foreground box-border`}
       onClick={onClick}
     >
       <div className="flex w-full items-center gap-2">
