5464664c70bf54d1137568f6babe1abc833f0f5b
ykjung <ykjung@scgs.co.kr>
2025-11-04 21:09:58 +0900

    화면 작업중 (메뉴 주석)

--- app/studio4/layout.jsx
+++ app/studio4/layout.jsx
@@ -29,3 +29,19 @@ export default function Layout({ children }) {
     </div>
   );
 }
+
+// 메뉴 (/)
+//   계좌
+//   보유종목 (클릭시 카카오 or 계좌목록 이동)
+//   추천종목
+// 보유종목리스트 (/held)
+// 추천종목리스트 (/recommend)
+// 상세 (/detail)
+// 매도 (/sell)
+// 매수 (/buy)
+
+// 카카오 로그인 (/login) (뒤로가기시 보유종목리스트으로 가야함)
+// 계좌목록 선택 (/account) (뒤로가기시 보유종목리스트으로 가야함)
+// 계좌 등록 (/account/add) (계좌목록으로 가야함)
+
+// npx shadcn@latest add button
